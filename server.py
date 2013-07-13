5afecdbeaefac16b0a9df822cd6d506af3dd81ca
Jay Reddy <jreddy@linkedin.com>
2013-07-12 21:59:24 -0700

    updated server

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
+import os
 from bottle import route, run
 
-@route('/hello')
-def routehello():
-    return "hello"
+@route("/")
+def hello_world():
+        return "Hello World!"
 
-run(host='localhost', port=8080, debug=True)
+run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))

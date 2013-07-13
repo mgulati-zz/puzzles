ff0de0ba3ce67a15f70778e824955dff577e9979
Jay Reddy <jreddy@linkedin.com>
2013-07-12 22:07:13 -0700

    added static file routing

--- server.py
+++ server.py
@@ -3,6 +3,10 @@ from bottle import route, run
 
 @route("/")
 def hello_world():
-        return "Hello World!"
+        return return static_file(index.html, root= '/static')
+
+@route("/static/<name>")
+def static(name):
+		return static_file(filename, root='/static')
 
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))

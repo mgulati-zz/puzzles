73b2d805a560f62e45f5341453d3150561a82ee8
Mayank Gulati <mynk.gulati@gmail.com>
2013-07-13 04:17:57 -0700

    Revert "Merge branch 'master' of https://github.com/mgulati/puzzles"
    This reverts commit 6be9ceb7b31529d1e5ac824a0fe1007d7604301d, reversing
    changes made to d26a9b1ecef449ea5d4c823ff6b5a42370be1788.

--- server.py
+++ server.py
@@ -1,86 +1,33 @@
 import os
-from bottle import route, run, static_file, Bottle, request, Response
+from bottle import route, run, Bottle
 
-import unicodedata
-from socketio import socketio_manage
-from socketio.namespace import BaseNamespace
-from gevent import monkey
-from socketio.server import SocketIOServer
-
-monkey.patch_all()
 
 app= Bottle()
-app.debug= True
-
-
 
 @app.route("/")
 def hello_world():
-	return static_file("index.html", root= 'static')
+        return static_file(index.html, root= '/static')
 
 @app.route("/static/<name>")
 def static(name):
-	return static_file(name, root='static')
+		return static_file(filename, root='/static')
 
 
-class MarkerMixin(object):
-	def __init__(self, *args, **kwargs):
-		super(MarkerMixin, self).__init__(*args, **kwargs)
-		if 'room' not in self.session:
-			self.session['room'] = ""  # a set of simple strings
-		if 'unlocked' not in self.session:
-			self.session['unlocked'] = False
+@app.route('/websocket')
+def handle_websocket():
+    wsock = request.environ.get('wsgi.websocket')
+    if not wsock:
+        abort(400, 'Expected WebSocket request.')
 
-	def join(self, room):
-		"""Lets a user join a room on a specific Namespace."""
-		if(self.session['room'] != room):
-			self.session['room'] = room
-			self.session['unlocked'] = False
+    while True:
+        try:
+            message = wsock.receive()
+            wsock.send("Your message was: %r" % message)
+        except WebSocketError:
+            break
+from gevent.pywsgi import WSGIServer
+from geventwebsocket import WebSocketHandler, WebSocketError
 
-	def unlock(self):
-		self.session['unlocked'] = True
-		room = self.session['room']
-		pkt = dict(type = "event",
-				   name = "unlocked")
-		for sessid, socket in self.socket.server.sockets.iteritems():
-			if 'room' not in socket.session:
-				continue
-			if room == socket.session['room'] and self.socket != socket:
-				socket.send_packet(pkt)
-
-class ChatNamespace(BaseNamespace, MarkerMixin):
-
-	def on_join(self, room):
-		self.room = room
-		self.join(room)
-		return True
-
-	def recv_disconnect(self):
-		self.disconnect(silent=True)
-		return True
-
-	def on_unlock(self):
-		self.unlock()
-		return True
-
-@app.route('/ws/')
-def socketio():
-	try:
-		socketio_manage(request.environ, {'/chat': ChatNamespace}, request)
-	except:
-		print("Exception while handling socketio connection")
-	return Response()
-
-SocketIOServer(('', os.environ.get('PORT', 5000)), app, transports=['xhr-polling'], resource="socket.io").serve_forever()
-
-
-
-#@get('/websocket', apply = [websocket])
-#def handle_websocket(ws):
-#    while True:
-#        msg = ws.receive()
-#        if msg is not None:
-#            ws.send(msg)
-#        else: break
-#run(host = '0.0.0.0', port=os.environ.get('PORT', 5000), reloader=True, server=TornadoWebSocketServer, handlers=tornado_handlers)
-#run(host='0.0.0.0', port=os.environ.get('PORT', 5000), server=GeventWebSocketServer, debug= True)
+server = WSGIServer(("0.0.0.0", 8080), app,
+                    handler_class=WebSocketHandler)
+server.serve_forever()
